997124a78b2d5ce6bc5aaf5e7e9a72c013bb13c3
Leon Nikitin <nlv@lab321.ru>
2017-06-28 16:51:33 +0600

    Импорт сделок https://trello.com/c/ZY8p5hXb/12-импорт-сделок
    Добавлен ИНН поставщика в спискок полей для определения дублей

--- custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_inn_winer_c.php
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_inn_winer_c.php
@@ -2,5 +2,13 @@
  // created: 2017-06-05 13:30:24
 $dictionary['Opportunity']['fields']['inn_winer_c']['inline_edit']='1';
 $dictionary['Opportunity']['fields']['inn_winer_c']['labelValue']='ИНН победителя';
+$dictionary['Opportunity']['fields']['inn_winer_c']['importable']='true';
+$dictionary['Opportunity']['fields']['inn_winer_c']['duplicate_merge']='enabled';
+$dictionary['Opportunity']['fields']['inn_winer_c']['duplicate_merge_dom_value']='1';
 
+$dictionary['Opportunity']['indices']['idx_opp_inn_winer_c'] = array (
+      'name' => 'idx_opp_inn_winer_c',
+      'type' => 'index',
+      'fields' => array ('inn_winer_c')
+);
  ?>
